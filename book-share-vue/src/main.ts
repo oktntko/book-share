079f1ed30214f41e75bcaec228c58bd3f84476e3
oktntko <oktntko@gmail.com>
2023-09-16 12:48:38 +0900

    やること整理

--- book-share-vue/src/main.ts
+++ book-share-vue/src/main.ts
@@ -48,3 +48,44 @@ window.addEventListener('unhandledrejection', (event: PromiseRejectionEvent) => 
 });
 
 app.config.errorHandler = handleError;
+
+/**
+ * vue create component programmatically
+ *
+ * 課題①: 作成したときに store などの状態が引き継がれない
+ * 解決方法: QUASAR を真似してみる
+ * - https://github.com/quasarframework/quasar/blob/dev/ui/src/plugins/Dialog.js
+ *   - ダイアログプラグインをインストール
+ * - https://github.com/quasarframework/quasar/blob/dev/ui/src/utils/private/global-dialog.js
+ *  - createChildApp
+ *    - https://github.com/quasarframework/quasar/blob/dev/ui/src/install-quasar.js
+ *      - vue#createApp で子アプリを作る
+ *      - 親アプリから子アプリにコンテキストを渡す ⭐ここが重要と思われる⭐
+ * やること:
+ *  - そもそも store を使っていないので、 store を使ってログイン状態を管理する
+ *  - モーダルを使って store が引き継がれないことを確認する
+ *  - プラグインのやり方にする必要がある？
+ *  - モーダルコンポーネントを作るときに、親アプリから子アプリにコンテキストを渡す ⭐ここが重要と思われる⭐
+ *
+ * 課題②: 複数作成したときにスタックを管理していない
+ *  - モーダルの場合
+ *    1. １個目を作る
+ *    2. ２個目を作る
+ *    3. １個目をエスケープキーで閉じる
+ *      => ２個目も閉じる
+ * 解決方法: ref とか使えばいい？？
+ *
+ * 機能面
+ * - ユーザ管理機能を消す
+ *    - プロフィール管理機能にする
+ *    - はじめてのサインアップ機能
+ *    - 二要素認証
+ *    - 自分のユーザを削除する機能
+ * - 投稿をストックする機能
+ * - 投稿にハートをつける機能
+ * - 本を探すから投稿を探すのリンクがうまくいってない
+ * - ランキングから本を探すのときと同じようにメニューを出す
+ * - 本を読んだだけじゃなくて本を読みたいもあり
+ * - 読んだ本の一覧、テーブルでよくないか？
+ * - Google Analytics を入れる
+ */
